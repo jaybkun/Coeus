1ea4ac15d174e63722a9c6206ff2506a680f07ae
Jay Barra <jay.f.barra@gmail.com>
2015-04-04 08:22:56 -0400

    still trying to debug openshift

--- server.js
+++ server.js
@@ -66,9 +66,9 @@ module.exports = app;
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
-var io = require('socket.io').listen(app.listen(server_port, server_ip_address, function(){
-    console.log("Listening on " + server_ip_address + ", server_port " + server_port)
-}));
+app.listen(server_port, server_ip_address, function() {
+    console.log("Listening on " + server_ip_address + ", server_port " + server_port);
+});
 
 io.sockets.on('connection', function(socket) {
     socket.emit('message', {message: "the weather channel"});
