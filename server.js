9673061d4d249509e69dd2d9bffc0aaf5d3230e6
Jay Barra <jay.f.barra@gmail.com>
2015-04-13 17:20:48 -0400

    trying to fix openshift failing to serve static components

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(require('node-compass')({mode: 'expanded'}));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/stylesheets')));
 
 app.use('/', routes);
 
