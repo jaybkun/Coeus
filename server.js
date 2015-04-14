a44942820d073d3d511557ba4aa5572635fa60d1
Jay Barra <jay.f.barra@gmail.com>
2015-04-13 21:11:43 -0400

    perhaps node-compass?

--- server.js
+++ server.js
@@ -7,20 +7,15 @@ var bodyParser = require('body-parser');
 var Promise = require('bluebird');
 var http = require('http');
 
-//var routes = require('./routes/index');
-
 var app = express();
 
-// uncomment after placing your favicon in /public
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(require('node-compass')({mode: 'expanded'}));
+//app.use(require('node-compass')({mode: 'expanded'}));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/stylesheets', express.static(path.join(__dirname, 'public/stylesheets')));
-//app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
